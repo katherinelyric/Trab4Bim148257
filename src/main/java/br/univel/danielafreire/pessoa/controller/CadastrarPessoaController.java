c4d362f1f36b298c9ce837b0887f97a9f9158c97
Danii <Danii@Samsung-Note>
2016-11-27 15:59:04 -0200

    Parte 7 - Cadastro via upload de arquivo xml. Adicão da parte grafica para o import do xml e alteração d aclasse CadastrarPessoaController para processar o arquivo xml e persistir os dados, dando retorno de sucesso (testado com 2 arquivos diferentes, funcional)

--- src/main/java/br/univel/danielafreire/pessoa/controller/CadastrarPessoaController.java
+++ src/main/java/br/univel/danielafreire/pessoa/controller/CadastrarPessoaController.java
@@ -5,18 +5,34 @@
  */
 package br.univel.danielafreire.pessoa.controller;
 
+
+import java.io.IOException;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.primefaces.model.UploadedFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import br.univel.danielafreire.model.PessoaModel;
 import br.univel.danielafreire.repository.PessoaRepository;
 import br.univel.danielafreire.usuario.controller.UsuarioController;
 import br.univel.danielafreire.uteis.Uteis;
+
  
 @Named(value="cadastrarPessoaController")
 @RequestScoped
-//Define que o registro será gravado através dos dados passados na interface de cadastro
+/*
+ * Define que o registro será gravado através dos dados passados na interface de cadastro
+ */
 public class CadastrarPessoaController {
  
 	@Inject
@@ -25,8 +41,23 @@ public class CadastrarPessoaController {
 	UsuarioController usuarioController;
 	@Inject
 	PessoaRepository pessoaRepository;
+	
+	/*
+	 * Parte para arquivos xml, getters e setters
+	 */
+	private UploadedFile file;
+	 
+	public UploadedFile getFile() {
+		return file;
+	}
  
-	//getter e setter do modelo pessoa
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+ 
+	/*
+	 * Getter e setter do modelo pessoa
+	 */
 	public PessoaModel getPessoaModel() {
 		return pessoaModel;
 	}
@@ -35,11 +66,13 @@ public class CadastrarPessoaController {
 		this.pessoaModel = pessoaModel;
 	}
  
-//Salva a pessoa cadastrada na interface ~input~
+/*
+ * Salva a pessoa cadastrada na interface ~input~
+ */
 	public void SalvarNovaPessoa(){
  
 		pessoaModel.setUsuarioModel(this.usuarioController.GetUsuarioSession());
-		//estabelece que a fonte dos dados do cadastro, é o input
+		//Estabelece que a fonte dos dados do cadastro, é o input
 		pessoaModel.setOrigemCadastro("I");
 		pessoaRepository.SalvarNovoRegistro(this.pessoaModel);
 		this.pessoaModel = null;
@@ -47,5 +80,63 @@ public class CadastrarPessoaController {
 		Uteis.MensagemInfo("Registro cadastrado com sucesso");
  
 	}
+	
+/*
+ * Metodo para fazer o upload do arquivo
+ */
+	 public void UploadRegistros() {
+
+		 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		 try {
+			 if(this.file.getFileName().equals("")){ //identifica se um arquivo foi selecionado para upload
+				 Uteis.MensagemAtencao("Nenhum arquivo selecionado!");
+				 return;
+			 }
+			 DocumentBuilder builder = factory.newDocumentBuilder();
+
+	                 Document doc = builder.parse(this.file.getInputstream());
+	                 Element element = doc.getDocumentElement();
+	                 NodeList nodes = element.getChildNodes();
+ 
+	                for (int i = 0; i < nodes.getLength(); i++) {
+	        	     Node node  = nodes.item(i);
+	        	     
+	        	    if(node.getNodeType() == Node.ELEMENT_NODE){
+	        		 Element elementPessoa =(Element) node;
+ 
+	        		 //Recupera os dados do arquivo xml, pela tag usada em cada atributo
+	        		 String nome  = elementPessoa.getElementsByTagName("nome").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String sexo = elementPessoa.getElementsByTagName("sexo").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String email = elementPessoa.getElementsByTagName("email").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String endereco = elementPessoa.getElementsByTagName("endereco").item(0).getChildNodes().item(0).getNodeValue();
+ 
+	        		 //Instancia de uma nova pessoa, e adição de cada dado passado, para essa instancia
+	        		 PessoaModel newPessoaModel = new PessoaModel();
+	        		 newPessoaModel.setUsuarioModel(this.usuarioController.GetUsuarioSession());
+	        		 newPessoaModel.setEmail(email);
+	        		 newPessoaModel.setEndereco(endereco);
+	        		 newPessoaModel.setNome(nome);
+	        		 newPessoaModel.setOrigemCadastro("X");
+	        		 newPessoaModel.setSexo(sexo);
+ 
+	        		 //Registra o que foi extraido do xml
+	        		 pessoaRepository.SalvarNovoRegistro(newPessoaModel);
+	        	   }
+	              }
+
+		     Uteis.MensagemInfo("Registros cadastrados com sucesso!");
+ 
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+			
+		} catch (SAXException e) {
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+		}
+ 
+	 }
  
 }
